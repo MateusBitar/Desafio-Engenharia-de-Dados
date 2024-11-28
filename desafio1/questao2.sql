ac28886d26f1085d3b471b941eca28d4fbe890ab
MateusBitar <129760150+MateusBitar@users.noreply.github.com>
2024-11-27 23:03:44 -0300

    Update questao2.sql

--- desafio1/questao2.sql
+++ desafio1/questao2.sql
@@ -1,1 +1,34 @@
+CREATE TABLE guest_checks (
+    guestCheckId BIGINT PRIMARY KEY,
+    chkNum INT,
+    opnBusDt DATE,
+    opnUTC DATETIME,
+    clsdUTC DATETIME,
+    clsdFlag BOOLEAN,
+    gstCnt INT,
+    subTtl FLOAT,
+    chkTtl FLOAT,
+    payTtl FLOAT,
+    locRef VARCHAR(50)
+);
+
+
+CREATE TABLE detail_lines (
+    guestCheckLineItemId BIGINT PRIMARY KEY,
+    guestCheckId BIGINT,
+    lineNum INT,
+    menuItemNum INT,
+    inclTax FLOAT,
+    prcLvl INT,
+    FOREIGN KEY (guestCheckId) REFERENCES guest_checks(guestCheckId)
+);
+
+
+CREATE TABLE taxes (
+    taxNum INT,
+    guestCheckId BIGINT,
+    txblSlsTtl FLOAT,
+    taxCollTtl FLOAT,
+    FOREIGN KEY (guestCheckId) REFERENCES guest_checks(guestCheckId)
+);
 
